b6fe595b034777b9d455730df1eef35b89b6ed41
Adam Mawlawi <adam@mradam.us>
2021-07-24 20:11:26 -0500

    fetch data on slug page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,49 @@
 import { API_URL } from '@/config/index';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '@/components/layout';
+import styles from '@/styles/Event.module.css';
 
 export default function EventPage({ evt }) {
-  console.log(evt);
+  const deleteEvent = (e) => {
+    console.log('delete');
+  };
 
   return (
     <Layout>
-      {' '}
-      <h1>{evt.name}</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href='#' className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delet Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} alt={evt.name} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Descriptio:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href='/events'>
+          <a className={styles.back}>{'<'} GO Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 }
