f2a29975fd7e41130691df61bcf6d21d4687cf41
Adam Mawlawi <adam@mradam.us>
2021-07-26 20:28:42 -0500

    create search component and add it to header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Search from '@/components/Search';
 import styles from '@/styles/Header.module.css';
 
 function Header() {
@@ -9,6 +10,7 @@ function Header() {
           <a>DJ Events</a>
         </Link>
       </div>
+      <Search />
       <nav>
         <ul>
           <li>
